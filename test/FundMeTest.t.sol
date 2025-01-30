d94c2532ad8123e31950823faa94e396e0aa9bc8
Eunum56 <msiaffu123@gmail.com>
2025-01-30 14:00:52 +0530

    Test fund funcion

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,13 +8,17 @@ import {FundMeScript} from "../script/FundMeScript.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user"); //Cheat code for creating a fake user address.
+    uint256 constant SEND_VALUE = 0.1 ether; //100000000000000000 wei 17 zeros.
+    uint256 constant STARTING_BALANCE = 100 ether;
 
     function setUp() external {
         FundMeScript fundMeScript = new FundMeScript();
         fundMe = fundMeScript.run();
+        vm.deal(USER, STARTING_BALANCE); // vm.deal is a cheat code to give the user some ether. or set user balance.
     }
 
-    function testMinumumDollarIsFive() public view {
+    function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 
@@ -25,5 +29,24 @@ contract FundMeTest is Test {
     function testPriceFeedVersion() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutETH() public {
+        vm.expectRevert(); // hey, after this line should revert or fail to pass the test!
+        fundMe.fund(); //sending 0 value
+    }
+
+    function testFundUpdateDataFunded() public {
+        vm.prank(USER); //The next transaction will be from this user.
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArray() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        address funder = fundMe.getFunders(0);
+        assertEq(funder, USER);
     }
 }
